2be6f03df0b9dc6acbb8ab8dacfe878f502ba556
William DeFreese <alexdefreese@gmail.com>
2015-04-20 00:43:48 -0400

    Added distutils setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name="emailprotectionslib",
+    packages=["emailprotectionslib"],
+    version="0.1.0",
+    description="Python library to interact with SPF, DKIM, and DMARC",
+    author="Alex DeFreese",
+    author_email="alexdefreese@gmail.com",
+    url="https://github.com/lunarca/pyemailprotectionslib",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
