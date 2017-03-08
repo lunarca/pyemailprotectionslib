4be433db245d35f0b98e2b7a82e8dbc48b70a192
Alex DeFreese <alexdefreese@gmail.com>
2017-03-08 10:06:29 -0500

    Upgrade to 0.8.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="emailprotectionslib",
     packages=["emailprotectionslib"],
-    version="0.8.2",
+    version="0.8.3",
     description="Python library to interact with SPF and DMARC",
     author="Alex DeFreese",
     author_email="alexdefreese@gmail.com",
